568b657e1e538e2011dbaf0a21b8ba981ebcc23d
Mateus Meneses <mateusppa10@gmail.com>
2024-06-18 19:22:33 -0300

    test husky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,7 +23,7 @@ const config = {
       }
     ],
     "@typescript-eslint/no-unused-vars": [
-      "warn",
+      "error",
       {
         "argsIgnorePattern": "^_"
       }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 
+const test = "";
+
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">

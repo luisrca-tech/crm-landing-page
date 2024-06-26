a173461ca1c805eb2ed4264b814d46cb7eef68c7
Luis Felipe <luisrochacruzalves@gmail.com>
2024-06-26 18:53:53 -0300

    🚸 Style: Change feature header accordingly comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import { Container } from '~/components/Container';
-import { Header } from '~/components/Header';
-
 export default function HomePage() {
-  return (
-    <Container>
-      <Header />
-    </Container>
-  );
+  return <main></main>;
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,40 +3,73 @@
 import Image, { type StaticImageData } from 'next/image';
 import LogoAttios from '/public/images/logo.svg';
 import Menu from '/public/images/menu.svg';
-import { Sheet, SheetContent, SheetDescription, SheetFooter, SheetOverlay, SheetTrigger } from './ui/sheet';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetOverlay,
+  SheetTitle,
+  SheetTrigger
+} from './ui/sheet';
 import Link from 'next/link';
 
 export function Header() {
   return (
-    <div className="flex justify-between p-4">
-      <Image src={LogoAttios as StaticImageData} alt='' />
+    <div className="flex justify-between p-4 xsm:hidden">
+      <Image priority src={LogoAttios as StaticImageData} alt="" />
 
       <Sheet>
-          <SheetTrigger>
-          <Image src={Menu as StaticImageData} alt='' />
+        <SheetTrigger>
+          <Image src={Menu as StaticImageData} alt="" />
         </SheetTrigger>
-        <SheetContent className="flex flex-col min-h-screen bg-blend-overlay">
+        <SheetOverlay className="animate-fadeIn" />
+        <SheetContent className="flex min-h-screen animate-slideIn flex-col">
+          <SheetHeader>
+            <SheetTitle>Attios Menu</SheetTitle>
+          </SheetHeader>
           <div className="flex-grow">
-            <SheetDescription>
-              <Link href="#">ínicio</Link>
-            </SheetDescription>
-            <SheetDescription>
-              <Link href="#">Depoimentos</Link>
-            </SheetDescription>
-            <SheetDescription>
-              <Link href="#">Preço</Link>
-            </SheetDescription>
-            <SheetDescription>
-              <Link href="#">Entre em contato</Link>
-            </SheetDescription>
+            <nav>
+              <ul>
+                <li>
+                  <SheetDescription>
+                    <Link href="#inicio">ínicio</Link>
+                  </SheetDescription>
+                </li>
+                <li>
+                  <SheetDescription>
+                    <Link href="#depoimentos">Depoimentos</Link>
+                  </SheetDescription>
+                </li>
+                <li>
+                  <SheetDescription>
+                    <Link href="#precos">Preço</Link>
+                  </SheetDescription>
+                </li>
+                <li>
+                  <SheetDescription>
+                    <Link href="#contato">Entre em contato</Link>
+                  </SheetDescription>
+                </li>
+              </ul>
+            </nav>
           </div>
 
-          <SheetFooter className="mt-auto flex flex-col space-y-2 items-center justify-center">
-            <Link href="#"><button className="w-[4.8125rem] h-10 rounded-xl border px-2 py-4 font-normal text-base-500 flex justify-center items-center">Entrar</button></Link>
-            <Link href="#"><button className="w-[14.5rem] h-10 rounded-xl border px-2 py-4 flex justify-center items-center bg-base-600 font-normal text-base-100">Cadastre-se gratuitamente</button></Link>
+          <SheetFooter className="mt-auto flex flex-col items-center justify-center space-y-2">
+            <Link href="#">
+              <button className="flex h-10 w-[4.8125rem] items-center justify-center rounded-xl border px-2 py-4 font-normal text-base-500">
+                Entrar
+              </button>
+            </Link>
+            <Link href="#">
+              <button className="flex h-10 w-[14.5rem] items-center justify-center rounded-xl border bg-base-600 px-2 py-4 font-normal text-base-100">
+                Cadastre-se gratuitamente
+              </button>
+            </Link>
           </SheetFooter>
         </SheetContent>
       </Sheet>
     </div>
-  )
+  );
 }

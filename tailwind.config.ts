fd199e91d7319c3cf047ee8d27329fa82d7fc1a3
Luis Felipe <luisrochacruzalves@gmail.com>
2024-06-26 14:19:41 -0300

    ➕ Chore: Install shadcn and use my tailwindconfig

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -39,6 +39,20 @@ export default {
         inter: ['var(--font-inter)'],
         opensans: ['var(--font-open_sans)']
       },
+        keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+       animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+       },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;

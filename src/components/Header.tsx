75115bb6f5fc58a22c7af9e356e70f45fca86f5f
Luis Felipe <luisrochacruzalves@gmail.com>
2024-06-28 16:40:11 -0300

    ✨ Feat: Update adjust with comments PR

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,62 +14,81 @@ import {
   SheetTrigger
 } from './ui/sheet';
 import Link from 'next/link';
+import { NavigationItems } from '~/constants/navigation-items';
 
 export function Header() {
   return (
-    <div className="flex justify-between p-4 xsm:hidden">
-      <Image priority src={LogoAttios as StaticImageData} alt="" />
+    <header>
+      <div className="flex justify-between p-4 md:flex lg:hidden">
+        <Image
+          priority
+          src={LogoAttios as StaticImageData}
+          alt="Logo da empresa Attios CRM"
+        />
 
-      <Sheet>
-        <SheetTrigger>
-          <Image src={Menu as StaticImageData} alt="" />
-        </SheetTrigger>
-        <SheetOverlay className="animate-fadeIn" />
-        <SheetContent className="flex min-h-screen animate-slideIn flex-col">
-          <SheetHeader>
-            <SheetTitle>Attios Menu</SheetTitle>
-          </SheetHeader>
-          <div className="flex-grow">
-            <nav>
-              <ul>
-                <li>
-                  <SheetDescription>
-                    <Link href="#inicio">ínicio</Link>
-                  </SheetDescription>
-                </li>
-                <li>
-                  <SheetDescription>
-                    <Link href="#depoimentos">Depoimentos</Link>
-                  </SheetDescription>
-                </li>
-                <li>
-                  <SheetDescription>
-                    <Link href="#precos">Preço</Link>
-                  </SheetDescription>
-                </li>
-                <li>
-                  <SheetDescription>
-                    <Link href="#contato">Entre em contato</Link>
-                  </SheetDescription>
-                </li>
-              </ul>
-            </nav>
+        <Sheet>
+          <SheetTrigger>
+            <Image src={Menu as StaticImageData} alt="Botão de abrir o menu" />
+          </SheetTrigger>
+          <SheetOverlay />
+          <SheetContent className="flex min-h-screen flex-col">
+            <SheetHeader>
+              <SheetTitle>Attios Menu</SheetTitle>
+            </SheetHeader>
+            <div className="flex-grow">
+              <nav>
+                <ul>
+                  {NavigationItems.map((item) => (
+                    <li key={item.href}>
+                      <SheetDescription>
+                        <Link href={item.href}>{item.label}</Link>
+                      </SheetDescription>
+                    </li>
+                  ))}
+                </ul>
+              </nav>
+            </div>
+            <SheetFooter className="mt-auto flex flex-col items-center justify-center space-y-2">
+              <Link href="#">
+                <button className="flex h-10 w-[4.8125rem] items-center justify-center rounded-xl border px-2 py-4 font-normal text-base-500">
+                  Entrar
+                </button>
+              </Link>
+              <Link href="#">
+                <button className="flex h-10 w-[14.5rem] items-center justify-center rounded-xl border bg-base-600 px-2 py-4 font-normal text-base-100">
+                  Cadastre-se gratuitamente
+                </button>
+              </Link>
+            </SheetFooter>
+          </SheetContent>
+        </Sheet>
+      </div>
+      <div className="hidden h-full w-full px-6 py-4 lg:flex 2xl:flex 3xl:flex">
+        <div className="flex gap-16 md:gap-4">
+          <Image
+            priority
+            src={LogoAttios as StaticImageData}
+            alt="Logo da empresa Attios CRM"
+          />
+          <div className="flex gap-4 md:gap-2">
+            {NavigationItems.map((item) => {
+              return (
+                <div className="px-2 py-4" key={item.href}>
+                  <Link href={item.href}>{item.label}</Link>
+                </div>
+              );
+            })}
           </div>
-
-          <SheetFooter className="mt-auto flex flex-col items-center justify-center space-y-2">
-            <Link href="#">
-              <button className="flex h-10 w-[4.8125rem] items-center justify-center rounded-xl border px-2 py-4 font-normal text-base-500">
-                Entrar
-              </button>
-            </Link>
-            <Link href="#">
-              <button className="flex h-10 w-[14.5rem] items-center justify-center rounded-xl border bg-base-600 px-2 py-4 font-normal text-base-100">
-                Cadastre-se gratuitamente
-              </button>
-            </Link>
-          </SheetFooter>
-        </SheetContent>
-      </Sheet>
-    </div>
+        </div>
+        <div className="ml-auto flex items-center justify-center gap-2">
+          <button className="flex h-10 items-center justify-center rounded-xl border border-base-200 bg-base-100 px-4 py-2  text-base font-normal tracking-[-0.2px] text-base-500">
+            Entrar
+          </button>
+          <button className="flex h-10 items-center justify-center rounded-xl border border-base-400 bg-base-600 px-2 py-4 text-base-100">
+            Cadastre-se gratuitamente
+          </button>
+        </div>
+      </div>
+    </header>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,61 +15,51 @@ export default {
         xsm: '30rem',
         '2xl': '87.5rem',
         '3xl': '100rem'
-      },
-      keyframes: {
-        'accordion-down': {
-          from: { height: '0' },
-          to: { height: 'var(--radix-accordion-content-height)' }
-        },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' }
-        },
-        fadeIn: {
-          '0%': { opacity: '0' },
-          '100%': { opacity: '1' }
-        },
-        slideIn: {
-          '0%': { transform: 'translateX(100%)' },
-          '100%': { transform: 'translateX(0)' }
-        }
-      },
-      animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
-        fadeIn: 'fadeIn 0.3s ease-out',
-        slideIn: 'slideIn 0.3s ease-out'
-      },
-      colors: {
-        base: {
-          100: 'rgb(var(--base-100) / <alpha-value>)',
-          200: 'rgb(var(--base-200) / <alpha-value>)',
-          300: 'rgb(var(--base-300) / <alpha-value>)',
-          400: 'rgb(var(--base-400) / <alpha-value>)',
-          500: 'rgb(var(--base-500) / <alpha-value>)',
-          600: 'rgb(var(--base-600) / <alpha-value>)'
-        },
-        'base-dark': {
-          100: 'rgb(var(--base-dark-100) / <alpha-value>)',
-          200: 'rgb(var(--base-dark-200) / <alpha-value>)',
-          300: 'rgb(var(--base-dark-300) / <alpha-value>)',
-          400: 'rgb(var(--base-dark-400) / <alpha-value>)',
-          500: 'rgb(var(--base-dark-500) / <alpha-value>)',
-          600: 'rgb(var(--base-dark-600) / <alpha-value>)'
-        },
-        blue: {
-          100: 'rgb(var(--blue-100) / <alpha-value>)',
-          200: 'rgb(var(--blue-200) / <alpha-value>)',
-          300: 'rgb(var(--blue-300) / <alpha-value>)',
-          400: 'rgb(var(--blue-400) / <alpha-value>)',
-          500: 'rgb(var(--blue-500) / <alpha-value>)',
-          600: 'rgb(var(--blue-600) / <alpha-value>)'
-        }
-      },
-      fontFamily: {
-        inter: ['var(--font-inter)'],
-        opensans: ['var(--font-open_sans)']
       }
+    },
+    colors: {
+      base: {
+        100: 'rgb(var(--base-100) / <alpha-value>)',
+        200: 'rgb(var(--base-200) / <alpha-value>)',
+        300: 'rgb(var(--base-300) / <alpha-value>)',
+        400: 'rgb(var(--base-400) / <alpha-value>)',
+        500: 'rgb(var(--base-500) / <alpha-value>)',
+        600: 'rgb(var(--base-600) / <alpha-value>)'
+      },
+      'base-dark': {
+        100: 'rgb(var(--base-dark-100) / <alpha-value>)',
+        200: 'rgb(var(--base-dark-200) / <alpha-value>)',
+        300: 'rgb(var(--base-dark-300) / <alpha-value>)',
+        400: 'rgb(var(--base-dark-400) / <alpha-value>)',
+        500: 'rgb(var(--base-dark-500) / <alpha-value>)',
+        600: 'rgb(var(--base-dark-600) / <alpha-value>)'
+      },
+      blue: {
+        100: 'rgb(var(--blue-100) / <alpha-value>)',
+        200: 'rgb(var(--blue-200) / <alpha-value>)',
+        300: 'rgb(var(--blue-300) / <alpha-value>)',
+        400: 'rgb(var(--blue-400) / <alpha-value>)',
+        500: 'rgb(var(--blue-500) / <alpha-value>)',
+        600: 'rgb(var(--blue-600) / <alpha-value>)'
+      }
+    },
+    fontFamily: {
+      inter: ['var(--font-inter)'],
+      opensans: ['var(--font-open_sans)']
+    },
+    keyframes: {
+      'accordion-down': {
+        from: { height: '0' },
+        to: { height: 'var(--radix-accordion-content-height)' }
+      },
+      'accordion-up': {
+        from: { height: 'var(--radix-accordion-content-height)' },
+        to: { height: '0' }
+      }
+    },
+    animation: {
+      'accordion-down': 'accordion-down 0.2s ease-out',
+      'accordion-up': 'accordion-up 0.2s ease-out'
     }
   },
   plugins: [require('tailwindcss-animate')]

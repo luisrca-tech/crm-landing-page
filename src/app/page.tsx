7ac1250483537f282f1de274d63e76ffc6b67df9
Luis Felipe <luisrochacruzalves@gmail.com>
2024-07-01 10:04:34 -0300

    ✨ Feat: Add security section in layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function HomePage() {
   return (
     <Container>
       <h1>CRM - LANDING PAGE</h1>
+
       <ClimbSafely />
     </Container>
   );
--- src/components/ClimbSafely.tsx
+++ src/components/ClimbSafely.tsx
@@ -1,25 +1,39 @@
+import Image from 'next/image';
 import Link from 'next/link';
+import { SecurityCertificates } from '~/constants/security-certificates';
 
 export function ClimbSafely() {
   return (
-    <section>
-      <div className="flex h-full w-full px-6 py-12"></div>
-      <div className="flex items-center justify-center">
-        <div className="flex flex-col px-6 pb-[50px] pt-[30px]">
-          <h2 className="font-opensans text-[32px] font-bold italic leading-[38px] tracking-[-0.5px] text-base-600">
-            Escale com <span className="text-base-300">segurança</span>
-          </h2>
-          <span className="mt-[6px] px-6 text-center text-base font-normal tracking-[-0.2px] text-base-600">
-            Attio é auditado e certificado pelas principais certificações da
-            industria.
-          </span>
-          <Link className="flex items-center justify-center" href="#">
-            <button className="mt-5 flex h-12 items-center justify-center rounded-xl border border-base-400 bg-base-600 px-3 py-4 text-base font-normal tracking-[-0.2px] text-base-100">
-              Comece gratuitamente
-            </button>
-          </Link>
+    <section className="relative mt-20 w-full">
+      <div className="absolute border border-base-200 py-6">
+        <div className="flex w-full flex-col items-center justify-center">
+          <div className="flex flex-col items-center justify-center">
+            <h2 className="font-opensans text-[28px] font-bold italic leading-[38px] tracking-[-0.5px] text-base-600">
+              Escale com <span className="text-base-300">segurança</span>
+            </h2>
+            <span className="mt-[6px] px-6 text-center text-sm font-normal tracking-[-0.2px] text-base-600">
+              Attio é auditado e certificado pelas principais certificações da
+              industria.
+            </span>
+            <Link className="flex items-center justify-center" href="#">
+              <button className="mt-5 flex h-12 items-center justify-center rounded-xl border border-base-400 bg-base-600 px-3 py-4 text-base font-normal tracking-[-0.2px] text-base-100">
+                Comece gratuitamente
+              </button>
+            </Link>
+          </div>
+          <div className="my-10 w-full border-t border-base-200"></div>
+          <div className="flex w-full flex-wrap items-center justify-center gap-8">
+            {SecurityCertificates.map((item) => {
+              return (
+                <Image
+                  key={item.id}
+                  src={item.image}
+                  alt="Certificados de segurança Attios"
+                />
+              );
+            })}
+          </div>
         </div>
-        <div></div>
       </div>
     </section>
   );

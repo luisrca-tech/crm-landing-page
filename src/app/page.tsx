a9e8995a11cc8d2ef628e585d6fc03a56d4de109
Luis Felipe <luisrochacruzalves@gmail.com>
2024-07-11 09:39:15 -0300

    🔥 Remove: Change file name and delete code at page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import { Container } from '~/components/Container';
-import { MadeForTeamWithBoxLine } from '~/components/blocks/MadeForTeam';
+import { MadeForTeamWithBoxLine } from '~/components/blocks/MadeForTeamSection';
 
 export default function HomePage() {
   return (
     <Container>
       <h1>CRM - LANDING PAGE</h1>
-      <div className="flex">
-        <MadeForTeamWithBoxLine />
-      </div>
+      <MadeForTeamWithBoxLine />
     </Container>
   );
 }

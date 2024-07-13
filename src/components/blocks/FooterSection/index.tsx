401bba62916528d7cdc2862d001bf92d196d5a63
Luis Felipe <luisrochacruzalves@gmail.com>
2024-07-13 11:09:52 -0300

    ♻️ Feature/CLPI0-16-footer
    Refacotr

--- src/components/blocks/FooterSection/index.tsx
+++ src/components/blocks/FooterSection/index.tsx
@@ -10,29 +10,26 @@ import { DesktopFooterSection } from './DesktopFooterSection';
 
 export function Footer() {
   return (
-    <div className="relative flex flex-col gap-10 pt-10">
-      <div className="flex justify-between">
-        <Image src={LogoAttio} alt="Logo da Attio CRM" />
-        <div className="flex gap-3 lg:hidden">
-          <Image
-            width={13}
-            height={13}
-            src={TwitterFooter}
-            alt="Twitter Logo"
-          />
-          <Image width={13} height={13} src={Dribbble} alt="Dribbble Logo" />
-          <Image width={13} height={13} src={Linkedin} alt="Linkedin Logo" />
+    <footer>
+      <div className="relative flex flex-col gap-10 pt-10">
+        <div className="flex items-center justify-between">
+          <Image src={LogoAttio} alt="Logo da Attio CRM" />
+          <div className="flex h-5 gap-3 lg:hidden">
+            <Image width={10} src={TwitterFooter} alt="Twitter Logo" />
+            <Image width={10} src={Dribbble} alt="Dribbble Logo" />
+            <Image width={10} src={Linkedin} alt="Linkedin Logo" />
+          </div>
+        </div>
+        <AccordionMobile />
+        <DesktopFooterSection />
+        <div className="flex items-center justify-center px-14 py-5 text-center">
+          <p className="max-w-[260px] text-[10px] leading-4 tracking-[-0.1px] text-base-400">
+            Este website foi construído durante uma mentoria fullstack. Nenhuma
+            informação aqui deve ser levada como verdadeira. Para mais
+            informações, acessar: www.mentoriafullstack.com{' '}
+          </p>
         </div>
       </div>
-      <AccordionMobile />
-      <DesktopFooterSection />
-      <div className="flex items-center justify-center px-14 py-5 text-center">
-        <p className="max-w-[260px] text-[10px] leading-4 tracking-[-0.1px] text-base-400">
-          Este website foi construído durante uma mentoria fullstack. Nenhuma
-          informação aqui deve ser levada como verdadeira. Para mais
-          informações, acessar: www.mentoriafullstack.com{' '}
-        </p>
-      </div>
-    </div>
+    </footer>
   );
 }

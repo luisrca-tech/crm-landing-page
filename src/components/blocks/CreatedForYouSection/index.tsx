a4f8decf60e874b5c7dc6553639a7f0fa36e8fb1
Luis Felipe <luisrochacruzalves@gmail.com>
2024-07-13 16:49:34 -0300

    🚚  feature/CLPI0-7-contact-us-section
    Move hero section to file and resolve imports conflits

--- src/components/blocks/CreatedForYouSection/index.tsx
+++ src/components/blocks/CreatedForYouSection/index.tsx
@@ -4,8 +4,8 @@ import Avatar from '/public/images/FristAvatar.png';
 import Box from '/public/images/Box.png';
 
 import { FeaturesBox } from './FeaturesBox';
-import { Testimonials } from '../Testimonials';
-import { HeadingSection } from '../HeadingSection';
+import { Testimonials } from '../../ui/Testimonials';
+import { HeadingSection } from '../../ui/HeadingSection';
 import { MobileCarousel } from './MobileCarousel';
 
 export function CreatedForYou() {

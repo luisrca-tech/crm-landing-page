54301360c765d7bfd5e517fff7371ec21b38b15d
Luis Felipe <luisrochacruzalves@gmail.com>
2024-07-13 10:15:32 -0300

    🐛 Feature/CLPI0-10-modele-seus-dados-e-fluxos---se
    Fix: Update the component following design at figma

--- src/components/blocks/YourDataAndFlowsSection/DesktopBoxView.tsx
+++ src/components/blocks/YourDataAndFlowsSection/DesktopBoxView.tsx
@@ -2,14 +2,15 @@ import Image from 'next/image';
 
 import Workspaces from '/public/images/WorkspacesDesktop.png';
 import Fluxo from '/public/images/FluxoDesktop.png';
+import Object from '/public/images/ObjectDesktop.png';
 
-import { BoxSection } from '../BoxSection';
 import { BoxToolsImages } from '~/constants/BoxToolImages';
+import { SectionsBoxes } from '../../ui/SectionsBoxes';
 
 export function DesktopBoxView() {
   return (
     <div>
-      <BoxSection>
+      <SectionsBoxes>
         <div className="flex gap-16 pt-10">
           <div className="flex pl-10 text-lg">
             <h2 className="font-normal">
@@ -28,31 +29,11 @@ export function DesktopBoxView() {
             alt="Imagem de um espaço virtual de trabalho"
           />
         </div>
-      </BoxSection>
-      <div className='grid max-h-[608px] grid-cols-5 gap-8 mt-8 grid-rows-4'>
-        <BoxSection className='w-full row-span-full col-span-3 '>
-          <div className="flex flex-col gap-16 pt-10">
-            <div className="flex pl-10 text-lg">
-              <h2 className="font-normal">
-                <span className="font-semibold">
-                  Sempre conecte aos seus dados.
-                </span>
-                <span className="ml-1">
-                  Sincronize continuamente com seu produto, despesas e
-                  relacionamentos em sua área de trabalho.
-                </span>
-              </h2>
-            </div>
-            <Image
-              className="w-full object-cover"
-              src={Workspaces}
-              alt="Imagem de um espaço virtual de trabalho"
-            />
-          </div>
-        </BoxSection>
-        <BoxSection className="flex-1 row-span-3 col-span-2">
-          <div className="flex flex-col gap-20 pt-10">
-            <div className="max-w-[29rem] pl-10 pr-1 text-lg">
+      </SectionsBoxes>
+      <div className="mt-8 grid grid-cols-5 grid-rows-4 gap-8 md:max-h-[420px] lg:max-h-[608px]">
+        <SectionsBoxes rootClassName="col-span-3 row-span-full w-full ">
+          <div className="flex flex-col pt-10">
+            <div className="flex max-w-[468px] pl-10 text-lg">
               <h2 className="font-normal">
                 <span className="font-semibold">
                   Um CRM para qualquer caso.
@@ -69,14 +50,24 @@ export function DesktopBoxView() {
               alt="Imagem de um espaço virtual de trabalho"
             />
           </div>
-        </BoxSection>
-        <BoxSection className='col-span-2'>
+        </SectionsBoxes>
+        <SectionsBoxes
+          rootClassName="col-span-2 row-span-3"
+          internalClassName="md:flex md:items-center md:justify-center"
+        >
+          <Image
+            className="w-full object-cover"
+            src={Object}
+            alt="Imagem de um espaço virtual de trabalho"
+          />
+        </SectionsBoxes>
+        <SectionsBoxes rootClassName="col-span-2">
           <div className="flex justify-around gap-3 px-6 py-7">
             {BoxToolsImages.map((box) => {
               return (
                 <div
                   key={box.id}
-                  className="flex h-16 w-16 items-center justify-center rounded-[9px] border border-[#E4E4E4] text-base-100"
+                  className="flex items-center justify-center rounded-[9px] border border-[#E4E4E4] text-base-100 lg:h-16 lg:w-16"
                 >
                   <div className="flex items-center justify-center gap-3">
                     <Image
@@ -89,8 +80,8 @@ export function DesktopBoxView() {
               );
             })}
           </div>
-        </BoxSection>
-      </div >
-    </div >
+        </SectionsBoxes>
+      </div>
+    </div>
   );
 }

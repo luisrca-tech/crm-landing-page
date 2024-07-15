fcbb1e99ffdd3672dae7ac030accc96e9f7b6ce2
Luis Felipe <luisrochacruzalves@gmail.com>
2024-07-15 09:12:25 -0300

    🚚 Feature/CLPI0-28-button-component
    Refactor: Move heroSection and resolve imports conflits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Container } from '~/components/Container';
 import { CreatedForYouWithBoxline } from '~/components/blocks/CreatedForYouSection';
-import { Hero } from '~/components/blocks/Hero';
-import { HeroImage } from '~/components/blocks/HeroImage';
+import { Hero } from '~/components/blocks/HeroSection/Hero';
+import { HeroImage } from '~/components/blocks/HeroSection/HeroImage';
 import { ReadyToBuildYourCrm } from '~/components/blocks/ReadyToBuildYourCrm';
 
 export default function HomePage() {

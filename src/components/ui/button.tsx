971539f66d15c8ad39172fc34b451761403ba31f
mateus <mateusppa10@gmail.com>
2024-08-02 15:37:20 -0300

    ♻️ Refactor: move button and buttonlinks to different folders

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,32 +1,9 @@
-import { cva, type VariantProps } from 'class-variance-authority';
+import { type VariantProps } from 'class-variance-authority';
 import Link from 'next/link';
 import * as React from 'react';
 
 import { cn } from '~/lib/utils';
-
-const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
-  {
-    variants: {
-      outline: {
-        primary:
-          'flex items-center justify-center rounded-xl border text-base font-normal border border-base-200 text-base-500 hover:opacity-80 h-12 px-4 py-3',
-        secondary:
-          'flex items-center justify-center rounded-xl border text-base font-normal border border-blue-500 text-base-200 hover:opacity-80 h-12 px-4 py-3',
-        tertiary:
-          'flex items-center justify-center rounded-xl border text-base font-normal border border-base-500 text-base-100 hover:opacity-80 h-12 px-4 py-3'
-      },
-      filled: {
-        primary:
-          'flex items-center justify-center rounded-xl bg-base-600  text-base font-normal text-base-100 hover:opacity-80 h-12 px-4 py-3',
-        secondary:
-          'flex items-center justify-center rounded-xl bg-blue-500 text-base font-normal text-base-100 hover:opacity-80 h-12 px-4 py-3',
-        tertiary:
-          'flex items-center justify-center rounded-xl bg-base-500 text-base font-normal text-base-100 hover:opacity-80 h-12 px-4 py-3'
-      }
-    }
-  }
-);
+import { buttonVariants } from './Button/ButtonCommonStyle';
 
 type CommonProps = VariantProps<typeof buttonVariants>;
 
